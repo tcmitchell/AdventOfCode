5793b11d638456479f699c0ca1affdb9fc71d87b
Tom Mitchell <tcmitchell@users.noreply.github.com>
2016-12-14 16:33:56 -0500

    2015 Day 2 parts 1 and 2

---
+++ 2015/day2/puzzle1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+
+def box_size(box_dims):
+    b = [int(d) for d in box_dims.split('x')]
+    b.sort()
+    return (2*b[0]*b[1] + 2*b[1]*b[2] + 2*b[0]*b[2] + b[0]*b[1])
+
+def load_data(filename):
+    with open(filename, 'rb') as f:
+        return sum([box_size(box) for box in f])
+
+def main(argv=None):
+    if not argv:
+        argv = sys.argv
+    print load_data(argv[1])
+
+if __name__ == '__main__':
+    main()
