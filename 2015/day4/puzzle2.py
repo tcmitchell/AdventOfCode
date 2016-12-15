d01fd35fb733a2b466ec6fcbca269ba24a02e1a6
Tom Mitchell <tcmitchell@users.noreply.github.com>
2016-12-14 20:05:59 -0500

    2015 Day 4 parts 1 and 2

---
+++ 2015/day4/puzzle2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import md5
+import sys
+
+def main(argv=None):
+    if not argv:
+        argv = sys.argv
+    seed = argv[1]
+    i = 1
+    while True:
+        x = md5.md5(seed + str(i)).hexdigest()
+        if x.startswith('000000'):
+            print 'Found 6 zeroes at', i
+            break
+        i += 1
+
+if __name__ == '__main__':
+    main()
