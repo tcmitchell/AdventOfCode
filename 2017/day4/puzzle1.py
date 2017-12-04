3c4aaab5a93be377f55c88867256f287dc6303e8
Tom Mitchell <tcmitchell@users.noreply.github.com>
2017-12-04 05:42:01 -0500

    2017 Day 4 Puzzle 1

---
+++ 2017/day4/puzzle1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# http://adventofcode.com/2017/day/4
+
+import sys
+
+
+def load_passphrases(datafile):
+    with open(datafile, 'rb') as f:
+        return [line.decode('utf-8').strip('\n') for line in f]
+
+
+def main(argv):
+    datafile = argv[1]
+    passphrases = load_passphrases(datafile)
+    valid = 0
+    invalid = 0
+    for phrase in passphrases:
+        words = phrase.split(' ')
+        word_set = set(words)
+        if len(words) == len(word_set):
+            valid += 1
+        else:
+            invalid += 1
+    print('Loaded %d passphrases' % (len(passphrases)))
+    print('Found %d valid' % (valid))
+    print('Found %d invalid' % (invalid))
+
+
+if __name__ == '__main__':
+    main(sys.argv)
