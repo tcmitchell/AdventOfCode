cbf4623b15c1b47701316c2fff643e5c71041a6d
Tom Mitchell <tcmitchell@users.noreply.github.com>
2017-12-09 09:46:59 -0500

    2017 Day 9 Puzzle 2

---
+++ 2017/day9/puzzle2.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+# http://adventofcode.com/2017/day/9
+
+import sys
+
+
+# Data is all on one line
+def load_input(datafile):
+    with open(datafile, 'rb') as f:
+        return f.read().decode('utf-8').rstrip()
+
+
+def filter_garbage(in_data):
+    out = []
+    in_size = len(in_data)
+    in_ptr = 0
+    in_garbage = False
+    gcount = 0
+    while in_ptr < in_size:
+        in_char = in_data[in_ptr]
+        if in_garbage:
+            if in_char == '!':
+                in_ptr += 1
+            elif in_char == '>':
+                in_garbage = False
+            else:
+                gcount += 1
+        else:
+            if in_char == '<':
+                in_garbage = True
+            else:
+                out.append(in_data[in_ptr])
+        in_ptr += 1
+    print('Garbage count = %d' % (gcount))
+    return ''.join(out)
+
+
+def score(group_data):
+    total = 0
+    gscore = 0
+    for c in group_data:
+        if c == '{':
+            gscore += 1
+        elif c == '}':
+            total += gscore
+            gscore -= 1
+        elif c == ',':
+            pass
+        else:
+            raise Exception('Unknown character in group data: %r' % (c))
+    print('Final gscore = %d' % (gscore))
+    return total
+
+
+def main(argv):
+    datafile = argv[1]
+    raw_data = load_input(datafile)
+    fdata = filter_garbage(raw_data)
+    # print(fdata)
+    data_score = score(fdata)
+    print('Score = %d' % (data_score))
+
+
+if __name__ == '__main__':
+    main(sys.argv)
