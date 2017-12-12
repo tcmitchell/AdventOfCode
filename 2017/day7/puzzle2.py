fa0327a3939eedeed120a01940acf55224114228
Tom Mitchell <tcmitchell@users.noreply.github.com>
2017-12-12 17:10:32 -0500

    2017 Day 7 Puzzle 2

--- 2017/day7/puzzle2.py
+++ 2017/day7/puzzle2.py
@@ -3,7 +3,7 @@
 # http://adventofcode.com/2017/day/7
 
 # Answer 913 is too high
-
+# Answer 137832 is too high
 
 import re
 import sys
@@ -18,32 +18,53 @@ class Program():
         self.name = name
         self.weight = weight
         self.supports = supports
-        self.depth = None
-        self.computed_weight = 0
+        self.computed_weight = None
 
-    def compute_weight_and_depth(self, all_programs):
-        if self.depth is not None:
-            print('%s is already computed' % (self.name))
+    def dump_tree(self, depth, all_programs):
+        format = '%s%s (%d)'
+        if self.computed_weight is None:
+            print('I have no computed weight: %r' % (self.name))
+        print(format % (depth * ' ', self.name, self.computed_weight))
+        for child in [all_programs[s] for s in self.supports]:
+            child.dump_tree(depth + 1, all_programs)
+
+    def dump_child_weights(self, all_programs):
+        print('Prog %s: %d' % (self.name, self.computed_weight))
+        for child in [all_programs[s] for s in self.supports]:
+            if child.weight == child.computed_weight:
+                print('  Child %s: %d (leaf)' %
+                      (child.name, child.computed_weight))
+            else:
+                print('  Child %s: %d' % (child.name, child.computed_weight))
+
+    def compute_weight(self, all_programs):
+        if self.computed_weight is not None:
+            # print('%s is already computed' % (self.name))
             return
         if not self.supports:
-            print('%s is a leaf' % (self.name))
-            self.depth = 0
+            # print('%s is a leaf' % (self.name))
             self.computed_weight = self.weight
             return
+        # Get the program instances for those I support
         progs = [all_programs[s] for s in self.supports]
         for p in progs:
-            p.compute_weight_and_depth(all_programs)
+            p.compute_weight(all_programs)
         weights = [p.computed_weight for p in progs]
-        # children = [(p.depth, p.computed_weight) for p in progs]
+        self.computed_weight = sum(weights)
+        if None in weights:
+            print(self.supports)
+            print(weights)
         test = set(weights)
         if len(test) != 1:
             print('Program %r is unbalanced: %r' % (self.name, weights))
             for p in progs:
-                print('Child %s: %d' % (p.name, p.computed_weight))
-        self.depth = progs[0].depth + 1
+                p.dump_child_weights(all_programs)
+            # self.dump_tree(0, all_programs)
+            # for p in progs:
+            #     print('Child %s: %d' % (p.name, p.computed_weight))
         self.computed_weight = sum(weights) + self.weight
-        print('%s has depth %d, computed weight %d' %
-              (self.name, self.depth, self.computed_weight))
+        # print('%s has computed weight %d' %
+        #       (self.name, self.computed_weight))
 
     @classmethod
     def from_input(cls, line):
@@ -97,7 +118,7 @@ def main(argv):
     all_programs = {p.name: p for p in programs}
     # print('%s: %r' % ('fwft', all_programs['fwft']))
     for p in all_programs.values():
-        p.compute_weight_and_depth(all_programs)
+        p.compute_weight(all_programs)
 
 
 if __name__ == '__main__':
