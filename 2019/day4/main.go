cba3567db3c3f5b935428910f43f2d34f9cce94a
Tom Mitchell <tcmitchell@users.noreply.github.com>
2019-12-04 21:54:29 -0500

    2019 day 4 part 1

--- 2019/day4/main.go
+++ 2019/day4/main.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-func loadInput(filename string) (int, int, error) {
+func loadInput(filename string) ([]int, []int, error) {
 	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, nil, err
@@ -27,16 +27,52 @@ func loadInput(filename string) (int, int, error) {
 	return hilo[0], hilo[1], nil
 }
 
-func passwdDigits(passwd int) ([]int, error) {
-	result := make([]int, 0)
-	for d := range strings.Split(string(passwd), "") {
-		digit, err := strconv.Atoi(d)
-		if err != nil {
-			return nil, err
-		}
-		result = append(result, digit)
+func validPassword(password []int) bool {
+	fmt.Printf("Testing %d\n", password)
+	if !(password[0] == password[1] ||
+		password[1] == password[2] ||
+		password[2] == password[3] ||
+		password[3] == password[4] ||
+		password[4] == password[5]) {
+		fmt.Printf("\tNo duplicate\n")
+		return false
 	}
-	return result, nil
+	if !(password[0] <= password[1] &&
+		password[1] <= password[2] &&
+		password[2] <= password[3] &&
+		password[3] <= password[4] &&
+		password[4] <= password[5]) {
+		fmt.Printf("\tNot increasing\n")
+		return false
+	}
+	if password[0] == 5 && password[1] >= 7 {
+		// Handcoded stop condition
+		return false
+	}
+	fmt.Printf("\tLooks good\n")
+	return true
+}
+
+func copyPassword(password []int) []int {
+	result := make([]int, len(password))
+	for i, p := range password {
+		result[i] = p
+	}
+	return result
+}
+
+func genPasswords(passwords [][]int, pos int, start int, password []int) [][]int {
+	if pos >= 6 {
+		if validPassword(password) {
+			passwords = append(passwords, copyPassword(password))
+		}
+		return passwords
+	}
+	for i := start; i <= 9; i++ {
+		password[pos] = i
+		passwords = genPasswords(passwords, pos+1, i, password)
+	}
+	return passwords
 }
 
 func part1(puzzleInput string) error {
@@ -44,8 +80,14 @@ func part1(puzzleInput string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Printf("lo: %s\n", passwdDigits(lo))
-	fmt.Printf("hi: %s\n", passwdDigits(hi))
+	fmt.Printf("%d -> %d", lo, hi)
+	passwords := make([][]int, 0)
+	password := make([]int, 6)
+	for i := 1; i < 6; i++ {
+		password[0] = i
+		passwords = genPasswords(passwords, 1, i, password)
+	}
+	fmt.Printf("Found %d valid passwords", len(passwords))
 	return nil
 }
 
--- 2019/day4/main_test.go
+++ 2019/day4/main_test.go
@@ -1,6 +1,6 @@
 package main
 
+import "testing"
+
 func TestRunProgram1(t *testing.T) {
-	program := []int{1, 0, 0, 0, 99}
-	expected := []int{2, 0, 0, 0, 99}
 }
