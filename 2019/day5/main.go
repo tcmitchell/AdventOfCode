ef41cf6d6c1ea678a52d927c9fd29720c70cb36b
Tom Mitchell <tcmitchell@users.noreply.github.com>
2019-12-05 22:03:42 -0500

    Template for day 5

---
+++ 2019/day5/main.go
@@ -0,0 +1,17 @@
+package main
+
+func part1(puzzleInput string) error {
+	return nil
+}
+
+func part2(puzzleInput string) error {
+	return nil
+}
+
+func main() {
+	err := part1("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	part2("input.txt")
+}
