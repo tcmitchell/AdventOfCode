49cdfe1aa48f140e3b8d996f0e1bc890995920f1
Tom Mitchell <tcmitchell@users.noreply.github.com>
2019-12-05 22:03:20 -0500

    Improve intcode for day5

--- 2019/intcode/intcode.go
+++ 2019/intcode/intcode.go
@@ -17,23 +17,92 @@ var opcodeEnd = 99
 // EndOfProgram indicates the program is complete
 var EndOfProgram = 99
 
+// paramMode figures out the paramater mode for the given
+// parameter from the instruction.
+func paramMode(instruction int, param int) int {
+	params := instruction / 100
+	for i := param; i > 1; i-- {
+		params = params / 10
+	}
+	return params % 10
+}
+
+// opcode gets the opcode from the instruction.
+// opcode is the rightmost two digits.
+func opcode(instruction int) int {
+	return instruction % 100
+}
+
+// getParameter gets the given parameter taking into account
+// the parameter mode.
+func getParameter(program Program, pc int, param int) (int, error) {
+	mode := paramMode(program[pc], param)
+	switch mode {
+	case 0:
+		// position mode
+		return program[program[pc+param]], nil
+	case 1:
+		// immediate mode
+		return program[pc+param], nil
+	default:
+		// Should return an error here
+		return 0, fmt.Errorf("Illegal parameter mode %d", mode)
+	}
+}
+
+func doAdd(program Program, pc int) (int, error) {
+	a, err := getParameter(program, pc, 1)
+	if err != nil {
+		return 0, err
+	}
+	b, err := getParameter(program, pc, 2)
+	if err != nil {
+		return 0, err
+	}
+	program[program[pc+3]] = a + b
+	return 4, nil
+}
+
+func doMultiply(program Program, pc int) (int, error) {
+	a, err := getParameter(program, pc, 1)
+	if err != nil {
+		return 0, err
+	}
+	b, err := getParameter(program, pc, 2)
+	if err != nil {
+		return 0, err
+	}
+	program[program[pc+3]] = a * b
+	return 4, nil
+}
+
 // Execute a single opcode of an Intcode program
-func Execute(intcode []int, pc int) (int, error) {
-	opcode := intcode[pc]
-	switch opcode {
+func Execute(program Program, pc int) (int, error) {
+	switch opcode(program[pc]) {
 	case opcodeAdd:
-		// Add
-		intcode[intcode[pc+3]] = intcode[intcode[pc+1]] + intcode[intcode[pc+2]]
-		return 4, nil
+		return doAdd(program, pc)
 	case opcodeMultiply:
-		// Multiply
-		intcode[intcode[pc+3]] = intcode[intcode[pc+1]] * intcode[intcode[pc+2]]
-		return 4, nil
+		return doMultiply(program, pc)
 	case opcodeEnd:
 		return EndOfProgram, nil
 	default:
-		return 0, fmt.Errorf("Unknown opcode %d", opcode)
+		return 0, fmt.Errorf("Unknown opcode %d", opcode(program[pc]))
 	}
+}
+
+func loadString(prog string) (Program, error) {
+	rawCode := strings.TrimSpace(prog)
+	strcode := strings.Split(rawCode, ",")
+	program := make(Program, len(strcode))
+	var err error
+	for i, s := range strcode {
+		program[i], err = strconv.Atoi(s)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return program, nil
+
 }
 
 // Load an intcode program from file
@@ -42,16 +111,7 @@ func Load(filename string) (Program, error) {
 	if err != nil {
 		return nil, err
 	}
-	rawCode := strings.TrimSpace(string(bytes))
-	strcode := strings.Split(rawCode, ",")
-	program := make(Program, len(strcode))
-	for i, s := range strcode {
-		program[i], err = strconv.Atoi(s)
-		if err != nil {
-			return nil, err
-		}
-	}
-	return program, nil
+	return loadString(string(bytes))
 }
 
 // Run executes an intcode program
