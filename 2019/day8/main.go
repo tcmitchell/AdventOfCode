086646e43f51fda2502048d3f1be988d9dd81462
Tom Mitchell <tcmitchell@users.noreply.github.com>
2019-12-17 20:00:35 -0500

    2019 day 8 part 2

--- 2019/day8/main.go
+++ 2019/day8/main.go
@@ -3,43 +3,78 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"strings"
 )
 
-func part1(puzzleInput string) error {
-	bytes, err := ioutil.ReadFile(puzzleInput)
+// const numCols int = 2
+// const numRows int = 2
+const numCols int = 25
+const numRows int = 6
+const layerSize int = numCols * numRows
+
+func loadLayers(filename string) ([]string, error) {
+	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	allLayers := strings.TrimSpace(string(bytes))
 	pixels := []rune(allLayers)
-	layerSize := 25 * 6
-	log.Printf("Loaded %d pixels\n", len(allLayers))
 	numLayers := len(allLayers) / layerSize
-	log.Printf("There are %d layers\n", numLayers)
 	layers := make([]string, numLayers)
 	for i := 0; i < numLayers; i++ {
 		layers[i] = string(pixels[i*layerSize : i*layerSize+layerSize])
 	}
+	return layers, nil
+}
+
+func part1(puzzleInput string) error {
+	layers, err := loadLayers(puzzleInput)
+	if err != nil {
+		return err
+	}
 	numZeroes := layerSize
 	answer := 0
-	for i, l := range layers {
-		log.Printf("Layer %d has %d pixels", i, len(l))
+	for _, l := range layers {
 		zeroes := strings.Count(l, "0")
 		if zeroes < numZeroes {
-			log.Printf("Layer %d has %d zeroes", i, zeroes)
 			numZeroes = zeroes
 			answer = strings.Count(l, "1") * strings.Count(l, "2")
 		}
 	}
 	fmt.Printf("Part 1: %d\n", answer)
-	// 2028 is too high
 	return nil
 }
 
+func pixelAt(layers []string, pos int) string {
+	for _, l := range layers {
+		switch l[pos] {
+		case '0':
+			return " "
+		case '1':
+			return "*"
+		}
+	}
+	// Transparent all the way through the layers
+	return "-"
+}
+
 func part2(puzzleInput string) error {
-	fmt.Printf("Part 2: No answer found\n")
+	layers, err := loadLayers(puzzleInput)
+	if err != nil {
+		return err
+	}
+	image := make([]string, layerSize)
+	for i := range image {
+		image[i] = pixelAt(layers, i)
+	}
+	fmt.Printf("Part 2:\n")
+	for i := range image {
+		if i%numCols == 0 {
+			fmt.Println()
+		}
+		fmt.Print(image[i])
+	}
+	fmt.Println()
 	return nil
 }
 
@@ -48,8 +83,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	part2("input.txt")
+	err = part2("input.txt")
 	if err != nil {
 		panic(err)
 	}
+	// err := part2("test2.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
 }
