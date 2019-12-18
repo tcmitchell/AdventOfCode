4410d96e1c07865d43ccbe7bfbac785f165109c4
Tom Mitchell <tcmitchell@users.noreply.github.com>
2019-12-18 09:18:54 -0500

    2019 day 9 part 2

--- 2019/day9/main.go
+++ 2019/day9/main.go
@@ -6,26 +6,37 @@ import (
 	"github.com/tcmitchell/AdventOfCode/2019/intcode"
 )
 
-func part1(puzzleInput string) error {
+func runBoost(filename string, input int) (int, error) {
 	inc := make(chan (int), 1)
-	inc <- 1
+	inc <- input
 	close(inc)
 	outc := make(chan (int), 30)
-	i, err := intcode.NewInterpreter(puzzleInput, inc, outc, "")
+	i, err := intcode.NewInterpreter(filename, inc, outc, "")
 	if err != nil {
-		return err
+		return 0, err
 	}
 	err = i.Run()
 	if err != nil {
+		return 0, err
+	}
+	return <-outc, nil
+}
+
+func part1(puzzleInput string) error {
+	answer, err := runBoost(puzzleInput, 1)
+	if err != nil {
 		return err
 	}
-	for out := range outc {
-		fmt.Printf("Part 1: %d\n", out)
-	}
+	fmt.Printf("Part 1: %d\n", answer)
 	return nil
 }
 
 func part2(puzzleInput string) error {
+	answer, err := runBoost(puzzleInput, 2)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Part 2: %d\n", answer)
 	return nil
 }
 
@@ -34,8 +45,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// err := part1("input.txt")
-	// if err != nil {
-	// 	panic(err)
-	// }
+	err = part2("input.txt")
+	if err != nil {
+		panic(err)
+	}
 }
