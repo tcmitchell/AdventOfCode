37def9ac275264d39c5de0c76f5f96772ae66bd0
Tom Mitchell <tcmitchell@users.noreply.github.com>
2020-11-28 13:15:39 -0500

    Day 14 part 1

--- 2019/day14/main.go
+++ 2019/day14/main.go
@@ -13,9 +13,17 @@ type Resource struct {
 	Name     string
 }
 
+func (resource *Resource) String() string {
+	return fmt.Sprintf("#<%d of %s>", resource.Quantity, resource.Name)
+}
+
 type Rule struct {
 	Product Resource
 	Resources []Resource
+}
+
+func (rule *Rule) String() string {
+	return fmt.Sprintf("#<Produce %d of %s>", rule.Product.Quantity, rule.Product.Name)
 }
 
 func parseResource(s string) (Resource, error) {
@@ -47,7 +55,7 @@ func parseRule(line string) (*Rule, error) {
 		return nil, err
 	}
 	// Make and return a rule
-	rule := Rule{product, nil}
+	rule := Rule{product, resources}
 	return &rule, nil
 }
 
@@ -56,7 +64,8 @@ func loadRules(ruleFile string) ([]*Rule, error) {
 	if err != nil {
 		return nil, err
 	}
-	lines := strings.Split(string(bytes), "\n")
+	data := strings.TrimSpace(string(bytes))
+	lines := strings.Split(data, "\n")
 	log.Printf("Loaded %d lines of rules", len(lines))
 	rules := make([]*Rule, len(lines))
 	for i, line := range lines {
@@ -72,19 +81,60 @@ func loadRules(ruleFile string) ([]*Rule, error) {
 	return rules, nil
 }
 
+func makeResourceRuleMap(rules []*Rule) map[string]*Rule {
+	result := make(map[string]*Rule)
+	for _, rule := range rules {
+		result[rule.Product.Name] = rule
+	}
+	return result
+}
+
+func produce(product Resource, rrm map[string]*Rule, warehouse *map[string]int) int {
+	if product.Name == "ORE" {
+		// Special case, we can use as much ORE as we want
+		if (*warehouse)["ORE"] > product.Quantity {
+			// There's already enough in the warehouse
+			return 0
+		} else {
+			(*warehouse)["ORE"] += product.Quantity
+			return product.Quantity
+		}
+	}
+	oreUsed := 0
+	// for each ingredient, ensure the warehouse has enough to produce the named resource
+	rule := rrm[product.Name]
+	for _, ingredient := range rule.Resources {
+		// does the warehouse have enough of the resource?
+		for ; ingredient.Quantity > (*warehouse)[ingredient.Name]; {
+			oreUsed += produce(ingredient, rrm, warehouse)
+		}
+		// Now remove from the warehouse the amount required to produce `product`
+		(*warehouse)[ingredient.Name] -= ingredient.Quantity
+	}
+	// Now add the product to the warehouse
+	(*warehouse)[rule.Product.Name] += rule.Product.Quantity
+	return oreUsed
+}
+
 func part1(ruleFile string) (int, error) {
 	rules, err := loadRules(ruleFile)
 	if err != nil {
 		return 0, err
 	}
 	log.Printf("Loaded %d rules", len(rules))
-	return 0, nil
+	// Map of resource name to production rule for that resource
+	rrm := makeResourceRuleMap(rules)
+	// Hold stock of resources that can be used in production
+	warehouse := make(map[string]int)
+	oreUsed := produce(Resource{1, "FUEL"}, rrm, &warehouse)
+	return oreUsed, nil
 }
 
 func main() {
 	fmt.Print("Hello, World!\n")
 	programFile := "input.txt"
-	programFile = "test-input1.txt"
+	//programFile = "test-input1.txt"
+	//programFile = "test-input5.txt"
 	ore, err := part1(programFile)
 	if err != nil {
 		log.Fatal(err)
