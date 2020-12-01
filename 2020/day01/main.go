005219ac71caf11dff2ec5b832b0d4b0c3b4bd26
Tom Mitchell <tcmitchell@users.noreply.github.com>
2020-12-01 17:45:58 -0500

    2020 day 1 part 2

--- 2020/day01/main.go
+++ 2020/day01/main.go
@@ -20,6 +20,29 @@ func strings2ints(input []string) ([]int, error) {
 	return result, nil
 }
 
+func part2(filename string) (int, error) {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return 0, err
+	}
+	input := strings.TrimSpace(string(bytes))
+	lines := strings.Split(input, "\n")
+	numbers, err := strings2ints(lines)
+	if err != nil {
+		return 0, err
+	}
+	for i, n := range numbers {
+		for j := i + 1; j < len(numbers); j++ {
+			for k := j + 1; k < len(numbers); k++ {
+				if n+numbers[j]+numbers[k] == 2020 {
+					return n * numbers[j] * numbers[k], nil
+				}
+			}
+		}
+	}
+	return 0, fmt.Errorf("No numbers summed to 2020")
+}
+
 func part1(filename string) (int, error) {
 	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -50,4 +73,9 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Part1: %d\n", p1)
+	p2, err := part2(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Part2: %d\n", p2)
 }
