496543f1090841ef1a737d56c38d8eca0ab0adf7
Tom Mitchell <tcmitchell@users.noreply.github.com>
2020-12-01 17:37:10 -0500

    2020 day 1 part 1

---
+++ 2020/day01/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func strings2ints(input []string) ([]int, error) {
+	result := make([]int, len(input))
+	for i, item := range input {
+		intItem, err := strconv.Atoi(item)
+		if err != nil {
+			return nil, err
+		}
+		result[i] = intItem
+	}
+	return result, nil
+}
+
+func part1(filename string) (int, error) {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return 0, err
+	}
+	input := strings.TrimSpace(string(bytes))
+	lines := strings.Split(input, "\n")
+	numbers, err := strings2ints(lines)
+	if err != nil {
+		return 0, err
+	}
+	for i, n := range numbers {
+		for j := i + 1; j < len(numbers); j++ {
+			if n+numbers[j] == 2020 {
+				return n * numbers[j], nil
+			}
+		}
+	}
+	return 0, fmt.Errorf("No numbers summed to 2020")
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+	filename := "input.txt"
+	//filename = "test-input1.txt"
+	p1, err := part1(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Part1: %d\n", p1)
+}
