0908bf5bc93cb931721d6b904d1d80849034a3fe
Tom Mitchell <tcmitchell@users.noreply.github.com>
2020-12-09 10:14:06 -0500

    2020 Day 9 part 2

--- 2020/day09/main.go
+++ 2020/day09/main.go
@@ -4,6 +4,7 @@ import (
 	"../aoc"
 	"fmt"
 	"log"
+	"math"
 )
 
 func validSum(sum int, nums []int) bool {
@@ -30,8 +31,59 @@ func part1(filename string, window int) (int, error) {
 	return 0, fmt.Errorf("no invalid sum found")
 }
 
-func part2(filename string) (int, error) {
-	return len(filename), nil
+func minIntSlice(numbers []int) int {
+	min := math.MaxInt64
+	for _, num := range numbers {
+		if num < min {
+			min = num
+		}
+	}
+	return min
+}
+
+func maxIntSlice(numbers []int) int {
+	max := math.MinInt64
+	for _, num := range numbers {
+		if num > max {
+			max = num
+		}
+	}
+	return max
+}
+
+func sumIntSlice(numbers []int) int {
+	sum := 0
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
+}
+
+func part2(filename string, invalidNumber int) (int, error) {
+	numbers, err := aoc.ReadFileOfInts(filename)
+	if err != nil {
+		return 0, err
+	}
+	// Find the position of the invalid number
+	invalidIndex := -1
+	for i, num := range numbers {
+		if num == invalidNumber {
+			invalidIndex = i
+		}
+	}
+	//log.Printf("Invalid index: %d", invalidIndex)
+	// Now find a group of numbers that sums to invalidNumber
+	for i := 0; i < invalidIndex; i++ {
+		for j := i + 1; j < invalidIndex; j++ {
+			numSlice := numbers[i:j]
+			if sumIntSlice(numSlice) == invalidNumber {
+				//log.Printf("Sum of %v", numSlice)
+				return minIntSlice(numSlice) + maxIntSlice(numSlice), nil
+			}
+		}
+	}
+
+	return 0, fmt.Errorf("no set of numbers found")
 }
 
 func main() {
@@ -47,7 +99,7 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Part 1: %d\n", p1)
-	p2, err := part2(filename)
+	p2, err := part2(filename, p1)
 	if err != nil {
 		log.Fatal(err)
 	}
