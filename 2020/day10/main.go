5834c4170c78d7bef52b9908974b9227798deded
Tom Mitchell <tcmitchell@users.noreply.github.com>
2020-12-10 15:11:54 -0500

    2020 Day 10 part 2

--- 2020/day10/main.go
+++ 2020/day10/main.go
@@ -35,8 +35,52 @@ func part1(filename string) (int, error) {
 	return countOnes * countThrees, nil
 }
 
+func countPathsToDevice(adapters []int, idx int, cache map[int]int) int {
+	adapterCount := len(adapters)
+	if idx+1 == adapterCount {
+		return 1
+	}
+	count := 0
+	//log.Printf("Examining %d", adapters[idx])
+	// Look at +1, +2, +3 to see if they are possible candidates
+	// If so, add each to the total, and go from there
+	current := adapters[idx]
+	for i := idx + 1; i < idx+4; i++ {
+		if i >= adapterCount {
+			break
+		}
+		diff := adapters[i] - current
+		//log.Printf("diff(%d, %d) = %d", joltages[i], current, diff)
+		if diff <= 3 {
+			value, ok := cache[i]
+			if ok {
+				count += value
+			} else {
+				count += countPathsToDevice(adapters, i, cache)
+			}
+		}
+	}
+	cache[idx] = count
+	return count
+}
+
 func part2(filename string) (int, error) {
-	return len(filename), nil
+	joltages, err := aoc.ReadFileOfInts(filename)
+	if err != nil {
+		return 0, err
+	}
+	sort.Ints(joltages)
+	//log.Printf("%v", joltages)
+	adapters := make([]int, len(joltages)+2)
+	copy(adapters[1:], joltages)
+	// add the first element, a zero
+	adapters[0] = 0 // The power port
+	// add the last element, the device, 3 + max value
+	adapters[len(joltages)+1] = joltages[len(joltages)-1] + 3
+	//log.Printf("%v", adapters)
+	cache := make(map[int]int)
+	total := countPathsToDevice(adapters, 0, cache)
+	return total, nil
 }
 
 func main() {
