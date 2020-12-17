0878ea009f33088aed9f8bdbd782d76a10ca3497
Tom Mitchell <tcmitchell@users.noreply.github.com>
2020-12-17 14:51:38 -0500

    2020 day 13 part 2

--- 2020/day13/main.go
+++ 2020/day13/main.go
@@ -58,12 +58,59 @@ func part1(filename string) (int, error) {
 	return (nextTime - departure) * nextBus, nil
 }
 
-func part2(filename string) (int, error) {
+type Bus struct {
+	Period int64
+	Offset int64
+}
+
+func p2LoadInput(filename string) ([]Bus, error) {
 	lines, err := aoc.ReadFileOfStrings(filename)
+	if err != nil {
+		return nil, err
+	}
+	result := make([]Bus, 0)
+	for i, b := range strings.Split(lines[1], ",") {
+		if b == "x" {
+			continue
+		}
+		busTime, err := strconv.Atoi(b)
+		if err != nil {
+			return nil, err
+		}
+		// Normalize the offset
+		i = i % busTime
+		result = append(result, Bus{int64(busTime), int64(i)})
+	}
+	return result, nil
+}
+
+func p2FindSolution(busses []Bus) int64 {
+	step := busses[0].Period
+	start := busses[0].Offset
+	for b := 1; b < len(busses); b++ {
+		//log.Printf("Step %d starting at %d", step, start)
+		for t := start; t < step*busses[b].Period; t += step {
+			if t%busses[b].Period == busses[b].Period-busses[b].Offset {
+				step *= busses[b].Period
+				start = t
+				break
+			}
+		}
+	}
+	//log.Printf("Result: %d", start)
+	return start
+}
+
+func part2(filename string) (int64, error) {
+	busses, err := p2LoadInput(filename)
 	if err != nil {
 		return 0, err
 	}
-	return len(lines), nil
+	//for _, b := range busses {
+	//	log.Printf("Bus %d at time T+%d", b.Period, b.Offset)
+	//}
+	answer := p2FindSolution(busses)
+	return answer, nil
 }
 
 func main() {
