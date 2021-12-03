f70a2ef17febe54fc87da01065ad0a5a79cceefe
Tom Mitchell <tcmitchell@users.noreply.github.com>
2021-12-03 14:01:43 -0500

    2021 day 3 part 2

--- 2021/day3/day3.py
+++ 2021/day3/day3.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import argparse
 import logging
 import sys
@@ -47,8 +48,49 @@ def puzzle1(data) -> int:
     return gamma * epsilon
 
 
-def puzzle2(commands) -> int:
-    return 0
+def freq(data: list[str], index: int) -> dict[str, int]:
+    """Get the character frequencies at the given index.
+    """
+    tally = defaultdict(int)
+    for d in data:
+        tally[d[index]] += 1
+    return tally
+
+
+def filter_data(data: list[str], index: int, value: str) -> list[str]:
+    return [d for d in data if d[index] == value]
+
+
+def find_ogr(data: list[str]) -> int:
+    for index in range(len(data[0])):
+        freqs = freq(data, index)
+        if freqs['1'] >= freqs['0']:
+            data = filter_data(data, index, '1')
+        else:
+            data = filter_data(data, index, '0')
+        if len(data) == 1:
+            break
+    return int(''.join(data[0]), base=2)
+
+
+def find_co2(data: list[str]) -> int:
+    for index in range(len(data[0])):
+        freqs = freq(data, index)
+        if freqs['0'] <= freqs['1']:
+            data = filter_data(data, index, '0')
+        else:
+            data = filter_data(data, index, '1')
+        if len(data) == 1:
+            break
+    return int(''.join(data[0]), base=2)
+
+
+def puzzle2(data) -> int:
+    ogr = find_ogr(data)
+    co2 = find_co2(data)
+    logging.debug('ogr = %r', ogr)
+    logging.debug('co2 = %r', co2)
+    return ogr * co2
 
 
 def main(argv):
