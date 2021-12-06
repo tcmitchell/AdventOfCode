c87f0e14220da1622c89eb8bb5d08964a8dde0cb
Tom Mitchell <tcmitchell@users.noreply.github.com>
2021-12-06 07:19:03 -0500

    2021 day 6

--- 2021/day6/day6.py
+++ 2021/day6/day6.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 import argparse
 import logging
 import sys
+from collections import defaultdict
 from typing import TextIO
 
 
@@ -23,16 +24,47 @@ def init_logging(debug=False):
     logging.basicConfig(format=msgFormat, datefmt=dateFormat, level=level)
 
 
-def load_input(fp: TextIO):
-    return fp.read()
+def load_input(fp: TextIO) -> list[int]:
+    return [int(x) for x in fp.read().strip().split(',')]
+
+
+def tick(stages: dict[int, int]) -> dict[int, int]:
+    result = {
+        0: stages[1],
+        1: stages[2],
+        2: stages[3],
+        3: stages[4],
+        4: stages[5],
+        5: stages[6],
+        6: stages[0] + stages[7],
+        7: stages[8],
+        8: stages[0]
+    }
+    return result
 
 
 def puzzle1(data) -> int:
-    return 0
+    stages: dict[int, int] = {}
+    for i in range(9):
+        stages[i] = 0
+    for stage in data:
+        stages[stage] += 1
+    for i in range(80):
+        logging.debug("On day %d there are %d fish.", i, sum(stages.values()))
+        stages = tick(stages)
+    return sum(stages.values())
 
 
 def puzzle2(data) -> int:
-    return 0
+    stages: dict[int, int] = {}
+    for i in range(9):
+        stages[i] = 0
+    for stage in data:
+        stages[stage] += 1
+    for i in range(256):
+        logging.debug("On day %d there are %d fish.", i, sum(stages.values()))
+        stages = tick(stages)
+    return sum(stages.values())
 
 
 def main(argv):
