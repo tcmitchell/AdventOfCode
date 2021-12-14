d016e83e0492c5a0a713ad81f09b41e353da0fdb
Tom Mitchell <tcmitchell@users.noreply.github.com>
2021-12-14 11:18:16 -0500

    2021 day 14 part 2

--- 2021/day14/day14.py
+++ 2021/day14/day14.py
@@ -23,42 +23,75 @@ def init_logging(debug=False):
     logging.basicConfig(format=msg_format, datefmt=date_format, level=level)
 
 
-def load_input(fp: TextIO) -> Tuple[list[str], dict[Tuple[str, str], str]]:
-    template = list(fp.readline().strip())
+def load_input(fp: TextIO) -> Tuple[str, dict[str, Tuple[str, str]]]:
+    template = fp.readline().strip()
     fp.readline()
     rules = {}
     for line in fp:
         parts = line.strip().split(' -> ')
-        rules[tuple(parts[0])] = parts[1]
+        rules[parts[0]] = (parts[0][0] + parts[1], parts[1] + parts[0][1])
     return template, rules
 
 
-def step_polymer(template: list[str], rules: dict[Tuple[str, ...], str]) -> list[str]:
-    pairs = [template[i:i+2] for i in range(len(template) - 1)]
-    new_polymer = []
-    for p in pairs:
-        poly_insert = rules[tuple(p)]
-        new_polymer.append(p[0])
-        new_polymer.append(poly_insert)
-    new_polymer.append(p[1])
+def template2polymer(template: str) -> dict[str, int]:
+    polymer = collections.defaultdict(int)
+    for i in range(len(template) - 1):
+        polymer[template[i:i+2]] += 1
+    return polymer
+
+
+def step_polymer(template: dict[str, int], rules: dict[str, Tuple[str, str]]) -> dict[str, int]:
+    new_polymer = collections.defaultdict(int)
+    for k, v in template.items():
+        for poly in rules[k]:
+            new_polymer[poly] += v
     return new_polymer
 
 
-def puzzle1(template: list[str], rules: dict[Tuple[str, ...], str]) -> int:
-    polymer = template
+def count_chars(polymer: dict[str, int], bonus: list[str]):
+    counter = collections.defaultdict(int)
+    for k, v in polymer.items():
+        for c in k:
+            counter[c] += v
+    for b in bonus:
+        counter[b] += 1
+    for k, v in counter.items():
+        counter[k] = v // 2
+    return counter
+
+
+def puzzle1(template: str, rules: dict[str, Tuple[str, str]]) -> int:
+    polymer = template2polymer(template)
+    bonus = [template[0], template[-1]]
     for i in range(10):
         polymer = step_polymer(polymer, rules)
+        # logging.debug("Freqs: %r", count_chars(polymer, bonus))
         # logging.debug("Len %d: %s", len(polymer), ''.join(polymer))
-    frequency = [(v, k) for k, v in collections.Counter(polymer).items()]
+
+    frequency = [(v, k) for k, v in count_chars(polymer, bonus).items()]
+    most = max(frequency, key=lambda x: x[0])
+    logging.debug("%s is most common at %d occurrences", most[1], most[0])
+    least = min(frequency, key=lambda x: x[0])
+    logging.debug("%s is least common at %d occurrences", least[1], least[0])
+    return most[0] - least[0]
+    # return 0
+
+
+def puzzle2(template: str, rules: dict[str, Tuple[str, str]]) -> int:
+    polymer = template2polymer(template)
+    bonus = [template[0], template[-1]]
+    for i in range(40):
+        polymer = step_polymer(polymer, rules)
+        # logging.debug("Freqs: %r", count_chars(polymer, bonus))
+        # logging.debug("Len %d: %s", len(polymer), ''.join(polymer))
+
+    frequency = [(v, k) for k, v in count_chars(polymer, bonus).items()]
     most = max(frequency, key=lambda x: x[0])
     logging.debug("%s is most common at %d occurrences", most[1], most[0])
     least = min(frequency, key=lambda x: x[0])
     logging.debug("%s is least common at %d occurrences", least[1], least[0])
     return most[0] - least[0]
 
-
-def puzzle2(template, rules) -> int:
-    return 0
 
 def main(argv=None):
     args = parse_args(argv)
