049cabf95a21dc52e5860eda97892121cd1454a4
Tom Mitchell <tcmitchell@users.noreply.github.com>
2023-12-03 14:57:10 -0500

    2023 day 3 part 2

--- 2023/day03/day03.py
+++ 2023/day03/day03.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 import argparse
 import logging
 import re
+from collections import defaultdict
 from typing import TextIO
 
 
@@ -29,6 +30,7 @@ def load_input(fp: TextIO):
 
 PART_NUMBER_RE = re.compile(r"\d+")
 SYMBOL_RE = re.compile(r"[^\d.]")
+GEAR_RE = re.compile(r"\*")
 
 
 def symbol_adjacent(schematic: list[str], row: int, start: int, end: int) -> bool:
@@ -49,7 +51,7 @@ def symbol_adjacent(schematic: list[str], row: int, start: int, end: int) -> boo
         return True
     return False
 
-
+# 535078
 def puzzle1(data) -> int:
     sum_part_numbers = 0
     for row, line in enumerate(data):
@@ -62,8 +64,47 @@ def puzzle1(data) -> int:
     return sum_part_numbers
 
 
+def record_gears(gears: dict[tuple[int, int],list[int]],
+                 schematic: list[str], row: int, start: int,
+                 end: int, part_number: int) -> None:
+    match = GEAR_RE.search(schematic[row], start, end)
+    while match is not None:
+        gears[(row, match.start())].append(part_number)
+        match = GEAR_RE.search(schematic[row], match.start() + 1, end)
+
+
+def gear_adjacent(gears: dict[tuple[int, int],list[int]],
+                  schematic: list[str], row: int, start: int,
+                  end: int, part_number: int) -> None:
+    max_pos = len(schematic[0])
+    # Check the row above
+    if row > 0:
+        record_gears(gears, schematic, row-1, max(0, start-1), min(max_pos, end+1), part_number)
+    # Check left
+    if start > 0:
+        record_gears(gears, schematic, row, start-1, start, part_number)
+    # Check right
+    if end < max_pos:
+        record_gears(gears, schematic, row, end, end+1, part_number)
+    # Check below
+    if row + 1 < len(schematic):
+        record_gears(gears, schematic, row+1, max(0, start-1), min(max_pos, end+1), part_number)
+
+
 def puzzle2(data) -> int:
-    return 0
+    gears: dict[tuple[int, int], list[int]] = defaultdict(list)
+    for row, line in enumerate(data):
+        match = PART_NUMBER_RE.search(line)
+        while match is not None:
+            part_number = int(match.group(0))
+            gear_adjacent(gears, data, row, match.start(), match.end(), part_number)
+            match = PART_NUMBER_RE.search(line, match.end())
+    # Now sum the gear ratios
+    ratio_sum = 0
+    for part_numbers in gears.values():
+        if len(part_numbers) == 2:
+            ratio_sum += part_numbers[0] * part_numbers[1]
+    return ratio_sum
 
 
 def main(argv=None):
