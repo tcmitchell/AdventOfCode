33c2c828208fd750f4466854a349b565682fb300
Tom Mitchell <tcmitchell@users.noreply.github.com>
2023-12-04 10:29:34 -0500

    2023 day 4 part 2

--- 2023/day04/day04.py
+++ 2023/day04/day04.py
@@ -50,8 +50,31 @@ def puzzle1(data: Collection[tuple[int, Collection[int], Collection[int]]]) -> i
     return total_points
 
 
-def puzzle2(data) -> int:
-    return 0
+def puzzle2TooSlow(data: Collection[tuple[int, Collection[int], Collection[int]]]) -> int:
+    total_cards = 0
+    card_lookup = {card[0]: card for card in data}
+    card_stack = list(data)
+    while card_stack:
+        card = card_stack.pop(0)
+        idx, winning, mine = card
+        match_count = len([True for n in mine if n in winning])
+        for i in range(idx+1, idx+match_count+1):
+            card_stack.append(card_lookup[i])
+        total_cards += 1
+    return total_cards
+
+
+def puzzle2(data: Collection[tuple[int, Collection[int], Collection[int]]]) -> int:
+    # Loop backwards through the cards computing how many cards each will win
+    card_lookup: dict[int, int] = {}
+    for idx, winning, mine in reversed(list(data)):
+        logging.debug("Precomputing card %d", idx)
+        match_count = len([True for n in mine if n in winning])
+        cards_won = match_count + sum([card_lookup[i] for i in range(idx+1, idx+match_count+1)])
+        card_lookup[idx] = cards_won
+    # Now figure out how many total cards
+    total_cards = sum(card_lookup.values()) + len(card_lookup)
+    return total_cards
 
 
 def main(argv=None):
