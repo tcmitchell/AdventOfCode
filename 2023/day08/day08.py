b395057bf6491c1c169f03e9a151fc606600b0a0
Tom Mitchell <tcmitchell@users.noreply.github.com>
2023-12-08 06:36:13 -0500

    2023 day 8 part 1

---
+++ 2023/day08/day08.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+import argparse
+import itertools
+import logging
+import re
+from typing import TextIO
+
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", type=argparse.FileType('r'),
+                        metavar="PUZZLE_INPUT")
+    parser.add_argument('-d', '--debug', action='store_true')
+    args = parser.parse_args(args)
+    return args
+
+
+def init_logging(debug=False):
+    msg_format = '%(asctime)s %(levelname)s %(message)s'
+    date_format = '%m/%d/%Y %H:%M:%S'
+    level = logging.INFO
+    if debug:
+        level = logging.DEBUG
+    logging.basicConfig(format=msg_format, datefmt=date_format, level=level)
+
+
+ALL_TOKENS_RE = re.compile("\w+")
+
+
+def load_input(fp: TextIO) -> tuple[str, dict[str, list[str]]]:
+    turns = fp.readline().strip()
+    # Skip blank line
+    fp.readline()
+    # Read node data
+    data = {}
+    for line in fp:
+        tokens = ALL_TOKENS_RE.findall(line)
+        data[tokens[0]] = [tokens[1], tokens[2]]
+    return turns, data
+
+
+def turns_to_indices(turns: str) -> list[int]:
+    turns = turns.replace('L', '0')
+    turns = turns.replace('R', '1')
+    return [int(t) for t in turns]
+
+def puzzle1(data: tuple[str, dict[str, list[str]]]) -> int:
+    turns, nodes = data
+    turn = itertools.cycle(turns_to_indices(turns))
+    cur_node = 'AAA'
+    end_node = 'ZZZ'
+    steps = 0
+    while cur_node != end_node:
+        cur_node = nodes[cur_node][next(turn)]
+        steps += 1
+    return steps
+
+
+def puzzle2(data) -> int:
+    return 0
+
+
+def main(argv=None):
+    args = parse_args(argv)
+
+    # Init logging
+    init_logging(args.debug)
+
+    data = load_input(args.input)
+    answer = puzzle1(data)
+    logging.info('Puzzle 1: %r', answer)
+    answer = puzzle2(data)
+    logging.info('Puzzle 2: %r', answer)
+
+
+if __name__ == '__main__':
+    main()
